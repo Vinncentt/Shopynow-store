fa3301b3c9fd2c5b8378cb4a68e76ea4b66681fd
sif1414 <ed14sif04@gmail.com>
2022-06-08 14:59:15 +0100

    init

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <a href="/">shopynow</a>
+      </header>
+      <main>List product</main>
+    </div>
+  );
+}
+
+export default App;
